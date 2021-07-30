156c9267d3f02b2f9229b7181924885192935d58
Damian Piętka <damianpietka@gmail.com>
2021-07-30 19:29:47 +0200

    The People Screen has been wiped

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -1,75 +1,20 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import {GiftedChat, Bubble} from 'react-native-gifted-chat';
-
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
+import {View, Text, StyleSheet} from 'react-native';
 
 const PeopleScreen = () => {
-  const [loading, setLoading] = useState(true);
-  const [messages, setMessages] = useState([]);
-  const ref = firestore().collection('Chats');
-
-  useEffect(() => {
-    return ref.orderBy('createdAt', 'desc').onSnapshot(querySnapshot => {
-      const list = [];
-      querySnapshot.forEach(doc => {
-        const {_id, createdAt, text, user} = doc.data();
-        list.push({
-          _id,
-          createdAt: createdAt.toDate(),
-          text,
-          user,
-        });
-      });
-
-      setMessages(list);
-
-      if (loading) {
-        setLoading(false);
-      }
-    });
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    );
-    const {_id, createdAt, text, user} = messages[0];
-    ref.add({
-      _id,
-      createdAt,
-      text,
-      user,
-    });
-  }, []);
-
   return (
-    <GiftedChat
-      renderBubble={props => {
-        return (
-          <Bubble
-            {...props}
-            textStyle={{
-              left: {
-                color: 'black',
-              },
-            }}
-            wrapperStyle={{
-              left: {
-                backgroundColor: 'lightgrey',
-              },
-            }}
-          />
-        );
-      }}
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-        name: 'Damian',
-      }}
-    />
+    <View style={styles.container}>
+      <Text>People Screen, Chats list</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default PeopleScreen;
