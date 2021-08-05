ccecea92065d6d8341dabaa6d2aa252ec73b65bd
Damian Piętka <damianpietka@gmail.com>
2021-08-05 22:41:42 +0200

    Only the user's own chats are displayed in PeopleScreen from now

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -6,21 +6,30 @@ import firestore from '@react-native-firebase/firestore';
 import ChatListElement from '../components/ChatListElement';
 
 const PeopleScreen = () => {
+  const authData = useSelector(state => state.auth);
+
   const [loading, setLoading] = useState(true);
   const [chats, setChats] = useState([]);
-  const ref = firestore().collection('Chats');
+  // const ref = firestore().collection('Chats');
+  const ref = firestore()
+    .collection('Users')
+    .doc(authData.userData.id)
+    .collection('Chats');
 
-  // fetch messages
+  // fetch Chats
   useEffect(() => {
     return ref.onSnapshot(querySnapshot => {
+      // List of user's chats
       const list = [];
       querySnapshot.forEach(documentSnapshot => {
+        // data inside the Chat doc
+        const {id, user} = documentSnapshot.data();
         list.push({
-          id: documentSnapshot.id,
+          id,
+          user,
         });
       });
       setChats(list);
-      console.log(list);
 
       if (loading) {
         setLoading(false);
