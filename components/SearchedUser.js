d8151c36e3f13417b043f39772b3d2e896d7c768
Damian Piętka <damianpietka@gmail.com>
2021-08-05 21:06:47 +0200

    A function responsible for creating a chat was created

--- components/SearchedUser.js
+++ components/SearchedUser.js
@@ -7,14 +7,49 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import firestore from '@react-native-firebase/firestore';
+import {useSelector, useDispatch} from 'react-redux';
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const SearchedUser = ({item}) => {
+  const authData = useSelector(state => state.auth);
+
+  const createChat = () => {
+    //create chat
+    firestore()
+      .collection('Chats')
+      .add({})
+      .then(chat => {
+        // add chat to logged user
+        firestore()
+          .collection('Users')
+          .doc(authData.userData.id)
+          .update({
+            chats: firestore.FieldValue.arrayUnion({
+              id: chat.id,
+              user: item.id,
+            }),
+          });
+
+        // add chat to selected user
+        firestore()
+          .collection('Users')
+          .doc(item.id)
+          .update({
+            chats: firestore.FieldValue.arrayUnion({
+              id: chat.id,
+              user: authData.userData.id,
+            }),
+          });
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Text>{item.email}</Text>
-      <TouchableOpacity style={styles.startChatButton}>
+      <TouchableOpacity onPress={createChat} style={styles.startChatButton}>
         <Text>Start chat</Text>
       </TouchableOpacity>
     </View>
