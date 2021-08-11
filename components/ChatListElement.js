f35999c0e8f718da8adfc3a46cdaec28bbdd815a
Damian Piętka <damianpietka@gmail.com>
2021-08-11 20:13:38 +0200

    The ChatListElement component has been restyled

--- components/ChatListElement.js
+++ components/ChatListElement.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {TouchableOpacity, Dimensions, Text, StyleSheet} from 'react-native';
+import {
+  TouchableOpacity,
+  Dimensions,
+  Text,
+  StyleSheet,
+  Image,
+  View,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 const windowWidth = Dimensions.get('window').width;
@@ -10,23 +17,53 @@ const ChatListElement = ({id, item}) => {
     <TouchableOpacity
       onPress={() => navigation.navigate('Chat', {id, username: item.username})}
       style={styles.container}>
-      <Text style={styles.username}>{item.username}</Text>
-      <Text>{item.email}</Text>
+      {/**To do -> change to user image */}
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://firebasestorage.googleapis.com/v0/b/chat-app-c20dd.appspot.com/o/defAvatar.jpg?alt=media&token=44212c24-deb3-41f2-9251-7931f53d18fa',
+        }}
+      />
+      <View style={styles.textBox}>
+        <Text style={styles.username}>{item.username}</Text>
+        <Text>{item.email}</Text>
+      </View>
+
+      {/**To do -> change to last message time */}
+      <Text style={styles.time}>21:22</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     width: windowWidth,
     padding: 15,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     borderBottomWidth: 1,
     borderColor: '#D1D9D9',
   },
+  textBox: {
+    marginLeft: 20,
+  },
+
   username: {
     fontWeight: '700',
-    fontSize: 18,
+    fontSize: 15,
+  },
+  time: {
+    position: 'absolute',
+    top: 18,
+    right: 18,
+    color: '#47597E',
+    fontSize: 13,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
   },
 });
 
