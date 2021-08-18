7854ea902fcaa5f704d23a389925eee0c40e9c26
Damian Piętka <damianpietka@gmail.com>
2021-08-18 18:25:05 +0200

    tab bar icons have been added

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import PeopleScreen from '../screens/PeopleScreen';
 import ProfileScreen from '../screens/ProfileScreen';
@@ -11,7 +12,25 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          if (route.name === 'People') {
+            return (
+              <Ionicons name="md-chatbox-ellipses" size={size} color={color} />
+            );
+          } else if (route.name === 'Search') {
+            return <Ionicons name="search" size={size} color={color} />;
+          } else if (route.name === 'Profile') {
+            return <Ionicons name="settings" size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#082032',
+        activeBackgroundColor: '#EEEEEE',
+        inactiveTintColor: '#787A91',
+      }}>
       <Tab.Screen name="People" component={ChatNavigator}></Tab.Screen>
       <Tab.Screen name="Search" component={SearchScreen}></Tab.Screen>
       <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
